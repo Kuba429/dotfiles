875eedd3655bf499a510f711b60d8ecc7fd63c23
Kuba429 <jakubzawadzki429@gmail.com>
2023-01-30 18:18:52 +0100

    colors

---
+++ nvim/lua/colors.lua
@@ -0,0 +1,19 @@
+vim.cmd("colorscheme monokai_ristretto")
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+vim.cmd("hi LineNr guibg=NONE ctermbg=NONE")
+vim.cmd("hi NvimTreeNormal guibg=NONE")
+vim.cmd("hi TabLineFill guibg=NONE")
+vim.cmd("hi TabLineSel guibg=NONE")
+vim.cmd("hi TelescopeNormal guibg=NONE")
+vim.cmd("hi TabLine guibg=#2e2e2e")
+vim.cmd("hi Visual guibg=#27292c")
+vim.cmd("hi FloatBorder guibg=ffffff")
+vim.cmd("hi NormalFloat guibg=#212121")
+vim.cmd("hi Pmenu guibg=#212121")
+vim.cmd("hi PmenuSel guibg=#202020")
+vim.cmd("hi PmenuThumb guibg=#cfd0d7")
+
+
+vim.cmd([[
+let g:sonokai_style = 'atlantis'
+]])
