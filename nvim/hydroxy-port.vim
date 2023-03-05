b2715e139f3e8220b8391e47f6ab2b7267254b17
Kuba429 <jakubzawadzki429@gmail.com>
2023-03-05 21:31:49 +0100

    hydroxy colorscheme port

---
+++ nvim/hydroxy-port.vim
@@ -0,0 +1,76 @@
+hi clear
+syntax reset
+let g:colors_name = "hydroxy-port"
+set background=dark
+set t_Co=256
+hi Normal guifg=#f6f6f6 ctermbg=NONE guibg=#161616 gui=NONE
+
+hi DiffText guifg=#d64969 guibg=NONE
+hi ErrorMsg guifg=#d64969 guibg=NONE
+hi WarningMsg guifg=#d64969 guibg=NONE
+hi PreProc guifg=#d64969 guibg=NONE
+hi Exception guifg=#d64969 guibg=NONE
+hi Error guifg=#d64969 guibg=NONE
+hi DiffDelete guifg=#d64969 guibg=NONE
+hi GitGutterDelete guifg=#d64969 guibg=NONE
+hi GitGutterChangeDelete guifg=#d64969 guibg=NONE
+hi cssIdentifier guifg=#d64969 guibg=NONE
+hi cssImportant guifg=#d64969 guibg=NONE
+hi Type guifg=#d64969 guibg=NONE
+hi Identifier guifg=#d64969 guibg=NONE
+hi PMenuSel guifg=#5bd4e6 guibg=NONE
+hi Constant guifg=#5bd4e6 guibg=NONE
+hi Repeat guifg=#5bd4e6 guibg=NONE
+hi DiffAdd guifg=#5bd4e6 guibg=NONE
+hi GitGutterAdd guifg=#5bd4e6 guibg=NONE
+hi cssIncludeKeyword guifg=#5bd4e6 guibg=NONE
+hi Keyword guifg=#5bd4e6 guibg=NONE
+hi IncSearch guifg=#b7d96e guibg=NONE
+hi Title guifg=#b7d96e guibg=NONE
+hi PreCondit guifg=#b7d96e guibg=NONE
+hi Debug guifg=#b7d96e guibg=NONE
+hi SpecialChar guifg=#b7d96e guibg=NONE
+hi Conditional guifg=#b7d96e guibg=NONE
+hi Todo guifg=#b7d96e guibg=NONE
+hi Special guifg=#b7d96e guibg=NONE
+hi Label guifg=#b7d96e guibg=NONE
+hi Delimiter guifg=#b7d96e guibg=NONE
+hi Number guifg=#b7d96e guibg=NONE
+hi CursorLineNR guifg=#b7d96e guibg=NONE
+hi Define guifg=#b7d96e guibg=NONE
+hi MoreMsg guifg=#b7d96e guibg=NONE
+hi Tag guifg=#b7d96e guibg=NONE
+hi String guifg=#b7d96e guibg=NONE
+hi MatchParen guifg=#b7d96e guibg=NONE
+hi Macro guifg=#b7d96e guibg=NONE
+hi DiffChange guifg=#b7d96e guibg=NONE
+hi GitGutterChange guifg=#b7d96e guibg=NONE
+hi cssColor guifg=#b7d96e guibg=NONE
+hi Function guifg=#ef8f5c guibg=NONE
+hi Directory guifg=#ffd363 guibg=NONE
+hi markdownLinkText guifg=#ffd363 guibg=NONE
+hi javaScriptBoolean guifg=#ffd363 guibg=NONE
+hi Include guifg=#ffd363 guibg=NONE
+hi Storage guifg=#ffd363 guibg=NONE
+hi cssClassName guifg=#ffd363 guibg=NONE
+hi cssClassNameDot guifg=#ffd363 guibg=NONE
+hi Statement guifg=#9797eb guibg=NONE
+hi Operator guifg=#9797eb guibg=NONE
+hi cssAttr guifg=#9797eb guibg=NONE
+
+
+hi Pmenu guifg=#f6f6f6 guibg=#121212
+hi SignColumn guibg=#161616
+hi Title guifg=#f6f6f6
+hi LineNr guifg=#909090 guibg=#161616
+hi NonText guifg=#646464 guibg=#161616
+hi Comment guifg=#646464 gui=italic
+hi SpecialComment guifg=#646464 gui=italic guibg=NONE
+hi CursorLine guibg=#121212
+hi TabLineFill gui=NONE guibg=#121212
+hi TabLine guifg=#909090 guibg=#121212 gui=NONE
+hi StatusLine gui=bold guibg=#121212 guifg=#f6f6f6
+hi StatusLineNC gui=NONE guibg=#161616 guifg=#f6f6f6
+hi Search guibg=#646464 guifg=#f6f6f6
+hi VertSplit gui=NONE guifg=#121212 guibg=NONE
+hi Visual gui=NONE guibg=#121212
--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,80 +1,167 @@
-vim.cmd([[
-source ~/.dotfiles/nvim/monokai_kuba.vim
-]])
-local Color, colors, Group, groups, styles = require('colorbuddy').setup()
+-- chaotic colorschemes
+if false then
+	vim.cmd([[
+	source ~/.dotfiles/nvim/monokai_kuba.vim
+	]])
+	local Color, colors, Group, groups, styles = require('colorbuddy').setup()
 
--- Color.new('background',  '#1e1e1e')
--- Color.new('background_lighter',  '#6f6f6f')
-Color.new('background',  '#18191e')
-Color.new('background_lighter', '#2f303a')
-Color.new('foreground',  '#FDFDFD')
-Color.new('red',         '#D84B82')
-Color.new('green',       '#D3F4AF')
-Color.new('yellow',      '#EFDC89')
-Color.new('blue',		 '#ADF6ED')
-Color.new('orange',		 '#DE9C8A')
-Color.new('purple',		 '#BCB7E2')
+	-- Color.new('background',  '#1e1e1e')
+	-- Color.new('background_lighter',  '#6f6f6f')
+	Color.new('background',  '#18191e')
+	Color.new('background_lighter', '#2f303a')
+	Color.new('foreground',  '#FDFDFD')
+	Color.new('red',         '#D84B82')
+	Color.new('green',       '#D3F4AF')
+	Color.new('yellow',      '#EFDC89')
+	Color.new('blue',		 '#ADF6ED')
+	Color.new('orange',		 '#DE9C8A')
+	Color.new('purple',		 '#BCB7E2')
 
-Group.new('Function', colors.green, nil, nil)
-Group.new('@function.builtin', groups.Function, groups.Function, groups.Function)
-Group.new('@function.call', groups.Function, groups.Function, groups.Function)
-Group.new('@method.call', colors.blue, nil, nil)
-Group.new('@number', colors.purple, nil , nil)
-Group.new('@string', colors.yellow, nil , nil)
-Group.new('@parameter', colors.foreground, nil , nil)
-Group.new('@operator', colors.foreground, nil , nil)
-Group.new('@punctuation.special', colors.foreground, nil , styles.bold)
-Group.new('@punctuation.bracket', colors.orange, nil , nil)
-Group.new('@punctuation.delimiter', colors.foreground, nil , nil)
-Group.new('@function.macro', colors.red, nil , styles.bold)
-Group.new('@constructor', colors.foreground, nil, styles.bold)
-Group.new('@property', colors.foreground, nil, nil)
-Group.new('@property.class', colors.red, nil, nil)
-Group.new('@property.id', colors.red, nil, nil)
-Group.new('@keyword', colors.red, nil, nil)
-Group.new('@variable', colors.foreground, nil , nil)
-Group.new('@conditional', groups["@keyword"], groups["@keyword"], groups["@keyword"])
-Group.new('@include', colors.purple, nil , nil)
-Group.new('@comment', colors.background_lighter, nil , nil)
+	Group.new('Function', colors.green, nil, nil)
+	Group.new('@function.builtin', groups.Function, groups.Function, groups.Function)
+	Group.new('@function.call', groups.Function, groups.Function, groups.Function)
+	Group.new('@method.call', colors.blue, nil, nil)
+	Group.new('@number', colors.purple, nil , nil)
+	Group.new('@string', colors.yellow, nil , nil)
+	Group.new('@parameter', colors.foreground, nil , nil)
+	Group.new('@operator', colors.foreground, nil , nil)
+	Group.new('@punctuation.special', colors.foreground, nil , styles.bold)
+	Group.new('@punctuation.bracket', colors.orange, nil , nil)
+	Group.new('@punctuation.delimiter', colors.foreground, nil , nil)
+	Group.new('@function.macro', colors.red, nil , styles.bold)
+	Group.new('@constructor', colors.foreground, nil, styles.bold)
+	Group.new('@property', colors.foreground, nil, nil)
+	Group.new('@property.class', colors.red, nil, nil)
+	Group.new('@property.id', colors.red, nil, nil)
+	Group.new('@keyword', colors.blue, nil, nil)
+	Group.new('@variable', colors.foreground, nil , nil)
+	Group.new('@conditional', groups["@keyword"], groups["@keyword"], groups["@keyword"])
+	Group.new('@include', colors.purple, nil , nil)
+	Group.new('@comment', colors.background_lighter, nil , nil)
 
-Group.new('@constant', colors.blue, nil, styles.bold)
-Group.new('@constant.builtin', groups["@constant"], groups["@constant"], groups["@constant"])
+	Group.new('@constant', colors.blue, nil, styles.bold)
+	Group.new('@constant.builtin', groups["@constant"], groups["@constant"], groups["@constant"])
 
-Group.new('@tag', colors.red, nil, nil)
-Group.new('@tag.delimiter', colors.white, nil, nil)
-Group.new('@tag.attribute', colors.green, nil, nil)
+	Group.new('@tag', colors.red, nil, nil)
+	Group.new('@tag.delimiter', colors.white, nil, nil)
+	Group.new('@tag.attribute', colors.green, nil, nil)
 
---Group.new("@type", colors.red, nil, nil)
-Group.new("@type.tag", colors.red, nil, nil)
---Group.new("@namespace", colors.blue, nil, styles.bold)
+	--Group.new("@type", colors.red, nil, nil)
+	Group.new("@type.tag", colors.red, nil, nil)
+	--Group.new("@namespace", colors.blue, nil, styles.bold)
 
-Group.new('Error', colors.red:light(), nil, styles.bold)
+	Group.new('Error', colors.red:light(), nil, styles.bold)
 
-Group.new("LineNr", colors.yellow, nil, styles.bold)
+	Group.new("LineNr", colors.yellow, nil, styles.bold)
 
-Group.new("LineNrAbove", colors.background:light(), nil, nil)
-Group.new("LineNrBelow", colors.background:light(), nil, nil)
--- Group.new("LineNrAbove", colors.background_lighter, nil, nil)
--- Group.new("LineNrBelow", colors.background_lighter, nil, nil)
+	Group.new("LineNrAbove", colors.background:light(), nil, nil)
+	Group.new("LineNrBelow", colors.background:light(), nil, nil)
+	-- Group.new("LineNrAbove", colors.background_lighter, nil, nil)
+	-- Group.new("LineNrBelow", colors.background_lighter, nil, nil)
 
-Group.new("Pmenu", nil, colors.background:light(), nil)
-Group.new("Normal", colors.foreground, colors.background, nil)
-Group.new("NonText", groups.normal, groups.normal, groups.normal)
-Group.new("EndOfBuffer", groups.LineNrAbove, groups.normal, groups.normal)
-Group.new("Visual", nil, colors.background:light(), nil)
+	Group.new("Pmenu", nil, colors.background:light(), nil)
+	Group.new("Normal", colors.foreground, colors.background, nil)
+	Group.new("NonText", groups.normal, groups.normal, groups.normal)
+	Group.new("EndOfBuffer", groups.LineNrAbove, groups.normal, groups.normal)
+	Group.new("Visual", nil, colors.background:light(), nil)
 
-Group.new("TabLine", nil, colors.background:light(), nil)
-Group.new("TabLineFill", nil, colors.background:light(), nil)
+	Group.new("TabLine", nil, colors.background:light(), nil)
+	Group.new("TabLineFill", nil, colors.background:light(), nil)
 
-Group.new("FloatBorder", nil, colors.background, nil) -- uncomment when using borders with lsp stuff
+	Group.new("FloatBorder", nil, colors.background, nil) -- uncomment when using borders with lsp stuff
 
-Group.new("NvimTreeCursorLine", colors.foreground, colors.background:light(), nil)
-Group.new("NvimTreeNormal", colors.foreground, nil, nil)
-Group.new("NvimTreeFolderName", colors.foreground, nil, nil)
+	Group.new("NvimTreeCursorLine", colors.foreground, colors.background:light(), nil)
+	Group.new("NvimTreeNormal", colors.foreground, nil, nil)
+	Group.new("NvimTreeFolderName", colors.foreground, nil, nil)
 
-Group.new("SignColumn", nil, colors.background, nil)
-Group.new("StatusLineNC", nil, colors.background:light(), nil)
-vim.cmd([[
-hi Normal guibg=NONE
-]])
+	Group.new("SignColumn", nil, colors.background, nil)
+	Group.new("StatusLineNC", nil, colors.background:light(), nil)
+	vim.cmd([[
+	hi Normal guibg=NONE
+	]])
+else
+	vim.cmd([[ 
+		source ~/.dotfiles/nvim/hydroxy-port.vim
+	]])
+	local Color, colors, Group, groups, styles = require('colorbuddy').setup()
+	Color.new('background',  '#161616')
+	Color.new('background_lighter', '#888888')
+	Color.new('foreground',  '#F6F6F6')
+	Color.new('red',         '#D64969')
+	Color.new('green',       '#B7D96E')
+	Color.new('yellow',      '#FFD363')
+	Color.new('blue',		 '#5BD4E6')
+	Color.new('orange',		 '#EF8F5C')
+	Color.new('purple',		 '#9797EB')
+
+	Group.new("Visual", nil, colors.background:light(),nil)
+
+	Group.new('Function', colors.green, nil, nil)
+	Group.new('@function.builtin', groups.Function, groups.Function, groups.Function)
+	Group.new('@function.call', groups.Function, groups.Function, groups.Function)
+	Group.new('@method.call', colors.green, nil, nil)
+	Group.new('@number', colors.purple, nil , nil)
+	Group.new('@string', colors.yellow, nil , nil)
+	Group.new('@parameter', colors.orange, nil , nil)
+	Group.new('@operator', colors.red, nil , nil)
+	Group.new('@punctuation.special', colors.background_lighter, nil , styles.bold)
+	Group.new('@punctuation.bracket', colors.background_lighter, nil , nil)
+	Group.new('@punctuation.delimiter', colors.background_lighter, nil , nil)
+	Group.new('@function.macro', colors.red, nil , styles.bold)
+	Group.new('@constructor', colors.blue, nil, styles.bold)
+	Group.new('@property', colors.foreground, nil, nil)
+	Group.new('@property.class', colors.red, nil, nil)
+	Group.new('@property.id', colors.red, nil, nil)
+	Group.new('@keyword', colors.blue, nil, nil)
+	Group.new('@keyword.return', colors.red, nil, nil)
+	Group.new('@keyword.export', colors.red, nil, nil)
+	Group.new('@variable', colors.foreground, nil , nil)
+	Group.new('@conditional', colors.red, groups["@keyword"], groups["@keyword"])
+	Group.new('@include', colors.red, nil , nil)
+	Group.new('@comment', colors.background_lighter, nil , nil)
+
+	Group.new('@constant', colors.purple, nil, styles.bold)
+	Group.new('@constant.builtin', groups["@constant"], groups["@constant"], groups["@constant"])
+
+	Group.new('@tag', colors.red, nil, nil)
+	Group.new('@tag.delimiter', colors.white, nil, nil)
+	Group.new('@tag.attribute', colors.green, nil, nil)
+
+	Group.new("@repeat", colors.red, nil,nil)
+
+	Group.new("@type", colors.foreground,nil,nil)
+	Group.new("@type.builtin", colors.blue,nil,nil)
+	--Group.new("@namespace", colors.blue, nil, styles.bold)
+
+	Group.new('Error', colors.red:light(), nil, styles.bold)
+
+	Group.new("LineNr", colors.yellow, nil, styles.bold)
+
+	Group.new("LineNrAbove", colors.background:light(), nil, nil)
+	Group.new("LineNrBelow", colors.background:light(), nil, nil)
+	-- Group.new("LineNrAbove", colors.background_lighter, nil, nil)
+	-- Group.new("LineNrBelow", colors.background_lighter, nil, nil)
+
+	Group.new("Pmenu", nil, colors.background:light(), nil)
+	Group.new("Normal", colors.foreground, colors.background, nil)
+	Group.new("NonText", groups.normal, groups.normal, groups.normal)
+	Group.new("EndOfBuffer", groups.LineNrAbove, groups.normal, groups.normal)
+	Group.new("Visual", nil, colors.background:light(), nil)
+
+	Group.new("TabLine", nil, colors.background:light(), nil)
+	Group.new("TabLineFill", nil, colors.background:light(), nil)
+
+	Group.new("FloatBorder", nil, colors.background, nil) -- uncomment when using borders with lsp stuff
+
+	Group.new("NvimTreeCursorLine", colors.foreground, colors.background:light(), nil)
+	Group.new("NvimTreeNormal", colors.foreground, nil, nil)
+	Group.new("NvimTreeFolderName", colors.foreground, nil, nil)
+
+	Group.new("SignColumn", nil, colors.background, nil)
+	Group.new("StatusLineNC", nil, colors.background:light(), nil)
+	vim.cmd([[
+	hi Normal guibg=NONE
+	]])
+end
+
 
